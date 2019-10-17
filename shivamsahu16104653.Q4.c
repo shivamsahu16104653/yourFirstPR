6855b719f3d5a64615397e88fdbe4713afcacbcd
shivamsahu16104653 <shivshivamsahu@gmail.com>
2019-10-17 22:16:41 +0530

    find greatest no between ten numbers

---
+++ shivamsahu16104653.Q4.c
@@ -0,0 +1,12 @@
+      #include<stdio.h>
+  int main()
+   {int i,a,max=-9999;
+   printf("Enter  two number");
+   for(i=1;i<=10;i++)
+   { scanf("%d",&a);
+     if(max<=a)
+      max=a;}
+    printf("Max=%d",max);
+    return 0;}
+
+
